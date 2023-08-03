2e65dadfd78988611641e09acaf9e398f8e6efb7
Linh Nguyen <nguyenthuylinh040205@gmail.com>
2023-08-03 11:06:13 +0700

    first commit

---
+++ lesson1/main.js
@@ -0,0 +1,54 @@
+let text = '';
+for(let i=0; i<10; i++){
+    text += 'This is number' + i +'<br>';
+    
+}
+document.getElementById("demo").innerHTML = text;
+
+
+var newArray = [
+    'JS',
+    'python',
+    'C++'
+]
+console.log(newArray.join(' - '));
+console.log(newArray.toString());
+console.log(newArray.push('html'));
+console.log(newArray.pop())
+
+
+//HOMEWORK
+//ex1
+var fruitStorage = ['Grape', 15];
+var fruitQuantity = fruitStorage[1];
+var fruitName = fruitStorage[0];
+console.log('I have' + ' ' + fruitQuantity + ' ' + fruitName );
+
+//ex2
+
+var textFruit
+for (let i = 0; i < 1; i++) {
+  
+  console.log("The fruit is " + fruitName);
+  if(fruitQuantity==1){
+    console.log("There are " + fruitQuantity + ' ' + fruitName)
+  }
+  else{
+    console.log("There are " + fruitQuantity + ' ' + fruitName + 's')
+  }
+
+}
+//ex2
+var myDog={
+    'name':'Ngáo',
+    'legs': 4,
+    'friends': ["Everything!"]
+}
+myDog.name = 'Husky';
+ console.log(myDog);
+myDog.color = 'Brown';
+console.log(myDog);
+delete myDog.friends;
+console.log(myDog);
+
+
